baa145f8a9908565a67e3497a8cd6f16c780cd4e
kennedyifeadi <kennedyifeadi@gmail.com>
2025-02-14 20:37:06 +0100

    Refactor InitialGameScreen component to include Squares utility

--- src/components/InitialGameScreen.jsx
+++ src/components/InitialGameScreen.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React from "react";
+import { Squares } from "./Utilities/Squares";
 
 export const InitialGameScreen = () => {
   return (
-    <div>InitialGameScreen</div>
-  )
-}
+    <div className="w-full h-full bg-[#060606] text-[#ffffff1a]">
+      <Squares
+        speed={0.1}
+        squareSize={40}
+        direction="diagonal" // up, down, left, right, diagonal
+        borderColor="#ffffff1a"
+        hoverFillColor="#222"
+      />
+    </div>
+  );
+};
