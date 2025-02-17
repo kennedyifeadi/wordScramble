92ce01c79afdd22759b1229c71dcd837ac278ecf
kennedyifeadi <kennedyifeadi@gmail.com>
2025-02-17 01:19:03 +0100

    Refactor InitialGameScreen component to include overflow-hidden and animated rules button

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { InitialGameScreen } from './components/InitialGameScreen'
 const App = () => {
   return (
-    <div className='w-full h-[100dvh] flex relative'>
+    <div className='w-full h-[100dvh] flex relative overflow-hidden'>
       <InitialGameScreen/>
     </div>
   )
