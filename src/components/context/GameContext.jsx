597c18a4474ff145c52c4eefccb67a97634c2838
kennedyifeadi <kennedyifeadi@gmail.com>
2025-02-18 23:05:15 +0100

    Refactor GameContext to include additional state variables and setters

--- src/components/context/GameContext.jsx
+++ src/components/context/GameContext.jsx
@@ -4,9 +4,21 @@ export const GameContext = createContext();
 
 export const GameProvider = ({ children }) => {
   const [timerState, setTimerState] = useState(false);
+  const [currentWordIndex, setCurrentWordIndex] = useState(0);
+  const [currentAttempt, setCurrentAttempt] = useState(1);
+  const [gameOver, setGameOver] = useState(false);
+  const [score, setScore] = useState(0);
+  const [hint, setHint] = useState('');
 
   return (
-    <GameContext.Provider value={{ timerState, setTimerState }}>
+    <GameContext.Provider value={{
+      timerState, setTimerState,
+      currentWordIndex, setCurrentWordIndex,
+      currentAttempt, setCurrentAttempt,
+      gameOver, setGameOver,
+      score, setScore,
+      hint, setHint
+    }}>
       {children}
     </GameContext.Provider>
   );
